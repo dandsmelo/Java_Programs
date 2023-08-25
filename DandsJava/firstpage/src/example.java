0db7e6265f36f3c3f5a6c2c19b87852bbc6e8463
Dandara <dands20melo@gmail.com>
2023-08-25 12:39:36 -0300

    Ajuste no exercício ordem crescente

---
+++ DandsJava/firstpage/src/example.java
@@ -0,0 +1,6 @@
+public class example {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        Carro c = new Carro(); //Carro é a classe , c é o objeto, new é a instância uma cópia da classe que ficara no objeto
+    }
+}
--- DandsJava/firstpage/src/ordemcresc.java
+++ DandsJava/firstpage/src/ordemcresc.java
@@ -17,21 +17,40 @@ public class ordemcresc {
             int temp = num1;
             num1 = num2;
             num2 = temp;
+            if (num2>num3){
+                temp = num2;
+                num2 = num3;
+                num3 = temp;
+                if(num1 > num2){
+                    temp = num1;
+                    num1 = num2;
+                    num2 = temp;
+                }
+            }
         } else if(num2 > num3){
             int temp = num2;
             num2 = num3;
             num3 = temp;
-        } else if(num1 > num3){
-            int temp = num1;
-            num1 = num3;
-            num3 = temp;
-        } else if(num1 > num2){
+            if(num1 > num3){
+                temp = num1;
+                num1 = num3;
+                num3 = temp;
+        } /*else if(num1 > num2){
             int temp = num1;
             num1 = num2;
             num2 = temp;
+        }*/
+        }else if(num1 > num3){
+            int temp = num1;
+            num1 = num3;
+            num3 = temp;
+            if(num1 > num2){
+                temp = num1;
+                num1 = num2;
+                num2 = temp;
+            }
         }
-
-        System.out.print(num1 + " " + num2 + " "+ num3);
-    }
+    System.out.print(num1 + " " + num2 + " "+ num3);
+}
 }
 //Arrumar código
