3a9a3b5d93b0069aa7efc0fcaf818c8735db3904
Dandara <dands20melo@gmail.com>
2023-09-15 17:38:39 -0300

    Finalização de exercício de tec de programação

--- DandsJava/firstpage/src/Funcionario.java
+++ DandsJava/firstpage/src/Funcionario.java
@@ -5,13 +5,48 @@ public class Funcionario {
     private double salarioBruto;
     private double desconto;
 
+    public Funcionario(String nome, double salarioBruto, double desconto) {
+        this.nome = nome;
+        this.salarioBruto = salarioBruto;
+        this.desconto = desconto;
+    }
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Funcionario fun = new Funcionario();
 
-        System.out.println("Nome do Fúmcionário: " + fun.nome);
-        System.out.println("Salário Bruto: " + fun.salarioBruto);
-        System.out.println("Desconto: " + fun.desconto);
+        System.out.println("Nome do Fúmcionário: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Salário Bruto: ");
+        double salarioBruto = sc.nextDouble();
+
+        System.out.println("Desconto: ");
+        double desconto = sc.nextDouble();
         
+        Funcionario f = new Funcionario(nome, salarioBruto, desconto);
+
+        f.mostrarDados();
+
+        System.out.print("\nDigite a porcentagem de aumento do salário: ");
+        double percentualAumento = sc.nextDouble();
+
+        f.aumentarSalario(percentualAumento);
+
+        System.out.println("\nDados do funcionário com aumento:");
+        f.mostrarDados();
+
     }
+    private double calcularSalarioLiquido() {
+        return salarioBruto - desconto;
+    }
+    private void aumentarSalario(double percentualAumento) {
+        salarioBruto *= (1 + percentualAumento / 100);
+    }
+    public void mostrarDados() {
+        double salarioLiquido = calcularSalarioLiquido();
+        System.out.println("Dados do funcionário:");
+        System.out.println("Nome: " + nome);
+        System.out.println("Salário Líquido: R$" + salarioLiquido);
+    }
+
 }
--- DandsJava/firstpage/src/Retangulo.java
+++ DandsJava/firstpage/src/Retangulo.java
@@ -4,7 +4,10 @@ public class Retangulo {
     public double altura;
     public double largura;
 
-    
+    public Retangulo(double altura, double largura){
+        this.altura = altura;
+        this.largura = largura;
+    }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -15,8 +18,15 @@ public class Retangulo {
         System.out.println("Digite a largura do retângulo: ");
         double largura = sc.nextDouble();
 
-        //Retangulo r = new Retangulo();
-        //double r.calcularArea(double altura, double largura);
+        Retangulo r = new Retangulo(altura, largura);
+
+        double area = r.calcularArea();
+        double perimetro = r.calcularPerimetro();
+        double diagonal = r.calcularDiagonal();
+
+        System.out.println("\nÁrea do retângulo: " + area);
+        System.out.println("Perímetro do retângulo: " + perimetro);
+        System.out.println("Diagonal do retângulo: " + diagonal);
 
 
     }
